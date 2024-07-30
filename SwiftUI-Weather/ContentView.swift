494f5c1f8010b2ea00e627050b33685eabe67d40
Bazee-code <eugeneobazee2@gmail.com>
2024-07-30 12:43:30 +0300

    Implement ZStack on home screen, add header and daily weather V & H stacks

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -9,13 +9,99 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack{
+            LinearGradient(colors: [.blue, .white], startPoint: .topLeading, endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+            VStack{
+                Text("Nairobi, Kenya")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding(.top, 20)
+                
+                VStack(spacing: 5){
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    Text("50°")
+                        .font(.system(size : 60, weight: .bold, design: .default))
+                        .foregroundColor(.white)
+                }
+                
+                HStack(spacing:20){
+                    VStack(spacing: 5){
+                        Text("Mon")
+                            .font(.system(size: 20, weight: .medium, design: .default))
+                            .foregroundColor(.white)
+                            .padding(.top, 10)
+                        Image(systemName: "cloud.sun.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40,height: 40)
+                        Text("32°")
+                            .foregroundColor(.white)
+                    }
+                    VStack(spacing: 5){
+                        Text("Tue")
+                            .font(.system(size: 20, weight: .medium, design: .default))
+                            .foregroundColor(.white)
+                            .padding(.top, 10)
+                        Image(systemName: "cloud.sun.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40,height: 40)
+                        Text("25°")
+                            .foregroundColor(.white)
+                    }
+                    VStack(spacing: 5){
+                        Text("Wed")
+                            .font(.system(size: 20, weight: .medium, design: .default))
+                            .foregroundColor(.white)
+                            .padding(.top, 10)
+                        Image(systemName: "cloud.sun.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40,height: 40)
+                        Text("27°")
+                            .foregroundColor(.white)
+                    }
+                    VStack(spacing: 5){
+                        Text("Thur")
+                            .font(.system(size: 20, weight: .medium, design: .default))
+                            .foregroundColor(.white)
+                            .padding(.top, 10)
+                        Image(systemName: "cloud.sun.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40,height: 40)
+                        Text("20°")
+                            .foregroundColor(.white)
+                    }
+                    VStack(spacing: 5){
+                        Text("Fri")
+                            .font(.system(size: 20, weight: .medium, design: .default))
+                            .foregroundColor(.white)
+                            .padding(.top, 10)
+                        Image(systemName: "cloud.sun.fill")
+                            .renderingMode(.original)
+                            .resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 40,height: 40)
+                        Text("15°")
+                            .foregroundColor(.white)
+                    }
+                }
+                Spacer()
+            }
+            
+            
         }
-        .padding()
     }
 }
 
